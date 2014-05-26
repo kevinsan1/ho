8313e0b5710af2d329c97d28e617f6d88c37dfde
kevinsan1 <kevin.mead1@hotmail.com>
2014-05-26 17:34:20 +0200

    Not working

--- heat_equation1D.m
+++ heat_equation1D.m
@@ -9,41 +9,50 @@ L = 1; % The system extends from (x)=(0) to (x)=(L)
 h = L/N;
 i = 0:(N-1); % 1:N
 x = h/2 + i*h;
+y = x;
 w = 0.2;
 xs = 0.5;
 ys = 0.5;
-tfinal = 2;
+coeff = tau/h^2;
+tfinal = .2;
 tau = .5*h;
 tsteps = ceil(tfinal/tau);
 %% Initialize Source function
 S = zeros(N); % Set all elements to zero
 xExponent = (x'-xs).^2;
-S = exp(-xExponent/w^2);
+yExponent = (y'-ys).^2;
+S = exp(-xExponent'/w^2);
+% mesh(x,S);
 deltaFunction = zeros(N,1);
 deltaFunction(round(N/2))=2;
 %% * Set up the Laplacian operator matrix
 lap = zeros(N);  % Set all elements to zero
-coeff = tau/h^2;
 for i=2:(N-1)
-    lap(i,i-1) = coeff;
-    lap(i,i) = -2*coeff;  % Set interior rows
-    lap(i,i+1) = coeff;
+    lap(i,i-1) = 1;
+    lap(i,i) = -2;  % Set interior rows
+    lap(i,i+1) = 1;
 end
 % Boundary conditions
-lap(1,1)=-coeff;
-lap(1,2)=coeff;
-lap(N,N)=-coeff;
-lap(N,N-1)=coeff;
+lap(1,1)=-1;
+lap(1,2)=1;
+lap(N,N)=-1;
+lap(N,N-1)=1;
 %% * Initialize Q-matrix
 Q = S; 
 %% * Compute A-matrix (Tn+1)=ATn
-dM = eye(N) - tau*lap; 
+dM = eye(N) - coeff*lap; 
 %% * Initialize loop and plot variables
-max_iter = tsteps;
+max_iter = .25*tsteps;
 time = linspace(0,max_iter*tau,max_iter);      % Record time for plots
 Qplot(:,1) = Q; % initial value
 %% * Loop over desired number of steps 
 for iter=2:max_iter
+    %* Compute new temperature
+    Q = dM\(Q);
+    Qplot(:,iter) = Q(:);
+end
+%%
+for iter=max_iter:tsteps
     %* Compute new temperature
     Q = dM\(Q);
     Qplot(:,iter) = Q(:);
